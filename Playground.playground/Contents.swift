635d7fded6d065fac906f628b9620684c56e9d9f
Ethan throndsen <Ethunderson1@gmail.com>
2015-12-07 11:43:13 -0700

    unfinished Playgrounds

---
+++ Playground.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+/*
+How to make documentation like java
+*/
+
+let label = "The width is "
+let width = 54
+let widthLabel = label + String(width)
+
+var welcomeMessage: String
+welcomeMessage = "New to swift"
+
+var red, green, blue: Double
+
+let ポポポ = "🀛🀛🀛"
+let meaningOfLife = 42
+let pi = 3.14159
+let anotherpi = 3+0.14159
+
+
